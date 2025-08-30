ec5fe8cdad1b7a25dbe5b053aa37b93b0e831a86
theYoshiDM <theyoshidm@gmail.com>
2025-08-30 14:48:38 -0700

    Add files via upload

---
+++ Robot.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private final RobotContainer m_robotContainer;
+  /*/
+  private SparkMax motor1 = new SparkMax(1, MotorType.kBrushless);
+  private SparkMax motor1F = new SparkMax(2, MotorType.kBrushless);
+  private SparkMax motor4 = new SparkMax(4, MotorType.kBrushless);
+  private SparkMax motor4F = new SparkMax(3, MotorType.kBrushless);
+  */
+
+  
+  public Robot() {
+    m_robotContainer = new RobotContainer();
+  }
+    
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+    
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    
+  }
+
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
+}
